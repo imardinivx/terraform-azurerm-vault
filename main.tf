7f9ed3bcae9f35a70ded88a65e9852bd8c848ffd
Ivan Mardini <ivan.mardini@verivox.com>
2026-04-17 10:41:34 +0200

    feat: new feature

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-resource "azurerm_key_vault" "this" {
+resource "azurerm_key_vault" "this" { // this is a new feature
   name                = var.name
   location            = var.location
   resource_group_name = var.resource_group_name
